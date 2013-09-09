9c68452197f1e29a0168fea15d48088c430cc17c
Kyle Kelley <kyle.kelley@rackspace.com>
2013-09-09 13:26:23 -0500

    Add init

---
+++ kyle/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+# -*- coding: utf-8 -*-
+'''Kyle
+'''
+
+__title__ = 'kyle'
+__version__ = '0.0.1'
+__build__ = 0x000001
+__author__ = 'Kyle Kelley'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2013 Kyle Kelley'
+
