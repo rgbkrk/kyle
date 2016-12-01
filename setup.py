43c300e6d5f051188fa0052b783ce5d1d3541fe5
Kyle Kelley <rgbkrk@gmail.com>
2016-12-01 13:22:35 -0800

    Bump version
    Thank you to our contributors, without which this release would not be
    possible.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ try:
 except ImportError:
     from distutils.core import setup
 
-version="0.0.1"
+version="0.0.2"
 
 if sys.argv[-1] == 'publish':
     print("Publishing Kyle {version}".format(version=version))
