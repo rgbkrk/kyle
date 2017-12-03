9eabb9957fc866688afefba28f2bd02ae6de73e9
Kyle Kelley <rgbkrk@gmail.com>
2017-12-02 17:22:22 -0800

    bump version

--- kyle/__init__.py
+++ kyle/__init__.py
@@ -70,8 +70,8 @@ cc::ckXWWXNNXXXNNXXNNNNNXNNNXXXXXXXXXKOkkdodololccccoooolcllollclooolloll.,oollc
 """
 
 __title__ = 'kyle'
-__version__ = '0.0.1'
-__build__ = 0x000001
+__version__ = '0.0.3'
+__build__ = 0x000003
 __author__ = 'Kyle Kelley'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2013 Kyle Kelley'
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ try:
 except ImportError:
     from distutils.core import setup
 
-version="0.0.2"
+version="0.0.3"
 
 if sys.argv[-1] == 'publish':
     print("Publishing Kyle {version}".format(version=version))
